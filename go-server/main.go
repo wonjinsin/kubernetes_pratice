71cfff22c459569f5c49880523b9b8a140abfe6c
Wonjinsin <slgktn92@gmail.com>
2025-03-23 19:19:08 +0900

    add. env

--- go-server/main.go
+++ go-server/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// get env variables
+	exampleEnv := os.Getenv("EXAMPLE_ENV")
+	if exampleEnv == "" {
+		log.Fatal("EXAMPLE_ENV is not set")
+	}
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World! %s", exampleEnv)
 	})
 	http.ListenAndServe(":8080", nil)
 }
