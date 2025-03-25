e95a2662f8ad8f69654a1e9c03f805401ae5b3ba
Wonjinsin <slgktn92@gmail.com>
2025-03-25 22:02:09 +0900

    add. volume

--- go-server/main.go
+++ go-server/main.go
@@ -14,8 +14,19 @@ func main() {
 		log.Fatal("EXAMPLE_ENV is not set")
 	}
 
+	exampleEnv2 := os.Getenv("EXAMPLE_ENV2")
+	if exampleEnv2 == "" {
+		log.Fatal("EXAMPLE_ENV2 is not set")
+	}
+
+	secretKey := os.Getenv("SECRET_KEY")
+	if secretKey == "" {
+		log.Fatal("SECRET_KEY is not set")
+	}
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World! %s", exampleEnv)
+		fmt.Fprintf(w, "Hello, World! %s %s", exampleEnv, exampleEnv2)
+		fmt.Fprintf(w, "Secret is %s", secretKey)
 	})
 	http.ListenAndServe(":8080", nil)
 }
